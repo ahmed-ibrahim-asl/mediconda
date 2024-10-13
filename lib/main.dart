b87fe7c5dfea72d0e6ba108857edf627a6d4d748
Mayar <mayarkhedeer02@gmail.com>
2024-10-13 22:30:30 +0300

    homescreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mediconda/core/app/mediconda_app.dart';
-import 'package:mediconda/features/home/presentation/pages/home_screen.dart';
+
 
 
 
