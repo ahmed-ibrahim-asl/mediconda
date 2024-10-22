7d40314963d688732aa68fd2f66998e32031930c
Mariam El Sheikh <81959358+mariam123570@users.noreply.github.com>
2024-10-22 15:23:23 +0300

    empty screen ui done

--- lib/features/empty/presentation/pages/empty_screen.dart
+++ lib/features/empty/presentation/pages/empty_screen.dart
@@ -1,10 +1,106 @@
 import 'package:flutter/material.dart';
 
-class EmptyScreen extends StatelessWidget {
-  const EmptyScreen({super.key});
+
+
+class EmptyNotificationsScreen extends StatelessWidget {
+  const EmptyNotificationsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            children: [
+              const Spacer(flex: 2),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.8,
+                child: AspectRatio(
+                  aspectRatio: 1,
+                  child: Image.asset(
+                    'no_notification.jpg',
+                    fit: BoxFit.scaleDown,
+                  ),
+                ),
+              ),
+              const Spacer(flex: 2),
+              ErrorInfo(
+                title: "Empty Notifications",
+                description:
+                "It looks like you don't have any notifications right now. We'll let you know when there's something new.",
+
+                btnText: "Check again",
+                press: () {},
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
+
+class ErrorInfo extends StatelessWidget {
+  const ErrorInfo({
+    super.key,
+    required this.title,
+    required this.description,
+    this.button,
+    this.btnText,
+    required this.press,
+  });
+
+  final String title;
+  final String description;
+  final Widget? button;
+  final String? btnText;
+  final VoidCallback press;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        constraints: const BoxConstraints(maxWidth: 400),
+        alignment: Alignment.center,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              title,
+              style: Theme.of(context)
+                  .textTheme
+                  .headlineSmall!
+                  .copyWith(fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 16),
+            Text(
+              description,
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 16 * 2.5),
+            button ??
+                ElevatedButton(
+                  onPressed: press,
+                  style: ElevatedButton.styleFrom(
+                      minimumSize: const Size(double.infinity, 48),
+                      backgroundColor: Colors.black,
+                      foregroundColor: Colors.white,
+                      shape: const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(8)))),
+                  child: Text(btnText ?? "Retry".toUpperCase()),
+                ),
+            const SizedBox(height: 16),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+
